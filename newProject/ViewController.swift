5be4271f2092e3743ae31da997073057bac317fd
ksoobean <ksoobean@gmail.com>
2019-07-15 13:22:18 +0900

    source tree에서 첫번째 commit.

---
+++ newProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newProject
+//
+//  Created by 김수빈 on 15/07/2019.
+//  Copyright © 2019 김수빈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
