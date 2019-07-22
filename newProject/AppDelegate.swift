d527ab15b1b83f7e2a414d301c945a25324d2c81
ksoobean <ksoobean@gmail.com>
2019-07-22 17:22:25 +0900

    Hello Swift -> 프로퍼티, 액션 메서드 사용, 버튼 화면 이동, 버튼 클릭 시 라벨 변경

--- newProject/AppDelegate.swift
+++ newProject/AppDelegate.swift
@@ -14,31 +14,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    // 앱이 런칭된 후 가장 먼저 실행되는 메소드로 사용자에게 보여지기 전에 초기화가 가능한 메소드
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("func application")
         return true
     }
-
+    
+    
+    // Actie -> Inactive 상태가 될 때
+    // 전화, 메세지 등의 인터럽트에 대한 처리, 게임의 경우 일시정지 로직을 구현해야하는 메서드
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("func applicationWillResignActive")
     }
 
+    
+    // 앱이 Background 상태가 되었을 때(언제든 suspended 상태로 전환 가능)
+    // 공유 자원을 해제하거나 유저 데이터 저장 등을 해야하는 메서드
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+        print("func applicationDidEnterBackground")
+    } 
 
+    
+    // Background -> Foreground 전환될 때(Active 상태가 되기 전)
+    // API로 앱의 상태를 갱신할 때 사용. 버전체크, 메인테이닝 체크 등등
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // 
+        print("func applicationWillEnterForeground")
     }
 
+    
+    // Active 상태로 전환되었을 때
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("func applicationDidBecomeActive")
     }
 
+    
+    // 종료될 때
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("func applicationWillTerminate")
     }
 
 
--- newProject/ViewController.swift
+++ newProject/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 아울렛 변수 : @IBOutlet으로 정의된 프로퍼티
+    @IBOutlet var uiLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+    
+    // 액션 메소드 : 객체의 이벤트를 제어할 때 사용하는 어노테이션과 함께 사용되는 메소드
+    @IBAction func buttonSayHello(_ sender: Any) {
+        self.uiLabel.text = "Hello Swift"
+    }
+    
+    
 
 }
 
