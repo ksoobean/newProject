f8026c93d8cd1717cd5a629a6cbc3de07ccae9cb
ksoobean <ksoobean@gmail.com>
2019-07-30 22:22:27 +0900

    2019.07.30. 계산기만들기

--- newProject/CalculatorView.swift
+++ newProject/CalculatorView.swift
@@ -10,7 +10,7 @@
 /// AC, +/-, % : tag 11, 12, 13
 /// +, -, x, /, = : tag 14, 15, 16, 17, 18
 
-// 주석쓰기 최대한 자세하게.. 제발... (단축키 : command + option  + /)
+// 주석쓰기 최대한 자세하게..(단축키 : command + option  + /)
 // 입력했던 값 결과창 위에다 표시 추가.
 // 모달. 1번에서 두번째 화면에 back 버튼 지우고 모달뷰 추가.
 
@@ -39,102 +39,152 @@ class UIRoundButton : UIButton {
 }
 
 class CalculatorView : UIViewController {
-
+    
     // 상속 클래스를 명확히 해줄 것.
-    @IBOutlet var number0: UIRoundButton!
-    @IBOutlet var number1: UIRoundButton!
-    @IBOutlet var number2: UIRoundButton!
-    @IBOutlet var number3: UIRoundButton!
-    @IBOutlet var number4: UIRoundButton!
-    @IBOutlet var number5: UIRoundButton!
-    @IBOutlet var number6: UIRoundButton!
-    @IBOutlet var number7: UIRoundButton!
-    @IBOutlet var number8: UIRoundButton!
-    @IBOutlet var number9: UIRoundButton!
-    @IBOutlet var dot: UIRoundButton!
-    @IBOutlet var clear: UIRoundButton!
-    @IBOutlet var reverse: UIRoundButton!
-    @IBOutlet var percent: UIRoundButton!
-    @IBOutlet var divide: UIRoundButton!
-    @IBOutlet var multiply: UIRoundButton!
-    @IBOutlet var minus: UIRoundButton!
-    @IBOutlet var plus: UIRoundButton!
-    @IBOutlet var equal: UIRoundButton!
+    // 코드 상으로 버튼 속성에 접근, 변경이 필요없으면 선언할 필요 없음!
+    //@IBOutlet var number0: UIRoundButton!
+    @IBOutlet var buttonPlus: UIRoundButton!
+    
+    var saveValue = "";
+    var newValue = "";
+    var calculator = "";
+    var result : Double = 0.0
     
     // 결과 보여주는 창
     @IBOutlet var showResult: UILabel!
-    // 계산 결과
-    var result : String = String()
-    
-    
-    // 최종 결과 표시 이전에 입력한 식(formula)
     @IBOutlet var showFormula: UILabel!
-    // 숫자, 연산 기호를 배열에 차례로 담음
-    var formula: [String] = []
+    var formula : String = String()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    
-    // 입력한 숫자
-    var currentNumber : Double = Double()
-    
-    // 클릭한 버튼의 text를 읽고 showResult Label에 보여줌
-    var isNumberClicked = false
-    @IBAction func isClickNumber(button : UIButton) {
-        // var clickednumber was never mutated sol) var -> let)
-        let clickednumber = button.titleLabel?.text
-        if isNumberClicked {
-            showResult.text! = showResult.text! + clickednumber!
-        } else {
-            // 숫자를 처음 클릭했을 때
-            showResult.text! = clickednumber!
-            isNumberClicked = true
-        }
-    }
-    
-    
-    // 숫자, dot을 제외한 버튼들이 선택됐을 때
-    /// AC, +/-, % : tag 11, 12, 13
-    /// +, -, x, /, = : tag 14, 15, 16, 17, 18
-    
-    var priority : Int = 0
-    @IBAction func clickedOperator(_ sender: UIRoundButton) {
-        
-        formula.append(showResult.text!)
-        
-        // 새로운 수를 입력받기 위함
-        isNumberClicked = false
-        
-        switch sender.tag {
-            
-        case 11 : //"AC"
-            showResult.text = "0"
-            formula.removeAll()
-            showFormula.text = ""
-            
-        case 14 : //"+"
-            formula.append("+")
-        case 15 : //"-"
-            formula.append("-")
-        case 16 : //"*"
-            formula.append("*")
-        case 17 : //"/"
-            formula.append("/")
-            
-        default : //"="
-            showFormula.text = formula.joined()
-            calculate(formula : formula)
-            showResult.text = result
-        }
     }
 
-    func calculate(formula : [String]) -> String{
+    @IBAction func isClickNumber(button : UIRoundButton) {
         
-        for i in 0...(formula.count - 1) {
-            
+        // 숫자 버튼을 클릭할 때 showResult 라벨에 표시함.
+        let clickednumber = (button.titleLabel!.text)!
+        
+        if showResult.text != "" {
+            // 숫자가 입력중일 때
+            showResult.text! += clickednumber
+        } else {
+            // 입력된 숫자가 없을 때
+            showResult.text = clickednumber
         }
-        return result
+        
+        // 저장된 값이 있을 때
+        if saveValue != "" {
+            // showResult 라벨의 숫자를 읽어서 newValue에 저장
+            
+            newValue = String(showResult.text!)
+            
+            // 계산을 위해 saveValue와 newValue의 값을 Double형으로 바꿈
+            let tmp1 : Double = Double(saveValue)!
+            let tmp2 : Double = Double(newValue)!
+            
+            // calculator에 해당하는 계산을 함수로 하고 결과를 result에 저장, showResult 라벨에 표시
+            if calculator == "+" {
+                formula.append(calculator)
+                formula.append(newValue)
+                result = plus(save : tmp1, new : tmp2)
+                showResult.text = String(result)
+            } else if calculator == "-" {
+                formula.append(calculator)
+                formula.append(newValue)
+                result = minus(save : tmp1, new : tmp2)
+                showResult.text = String(result)
+            } else if calculator == "*" {
+                formula.append(calculator)
+                formula.append(newValue)
+                result = multiply(save : tmp1, new : tmp2)
+                showResult.text = String(result)
+            } else if calculator == "/" {
+                formula.append(calculator)
+                formula.append(newValue)
+                result = divide(save : tmp1, new : tmp2)
+                showResult.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func isOptClick(button : UIRoundButton) {
+        let clickedOpt = button.tag
+        /// AC, +/-, % : tag 11, 12, 13
+        /// +, -, x, /, = : tag 14, 15, 16, 17, 18
+        switch clickedOpt {
+        case 11 :   // AC
+            showResult.text = "0"
+            calculator = ""
+        case 12 :   // +/-
+            let tmp : Double = -Double(String(showResult.text!))!
+            saveValue = String(tmp)
+            showResult.text = saveValue
+            newValue = ""
+        case 13 :   // %
+            let tmp : Double = Double(String(showResult.text!))! * 0.01
+            saveValue = String(tmp)
+            showResult.text = saveValue
+            newValue = ""
+        case 14 :   // +
+            saveValue = String(showResult.text!)
+            calculator = "+"
+            showResult.text = ""
+            newValue = ""
+        case 15 :   // -
+            saveValue = String(showResult.text!)
+            calculator = "-"
+            showResult.text = ""
+            newValue = ""
+        case 16 :   // *
+            saveValue = String(showResult.text!)
+            calculator = "*"
+            showResult.text = ""
+            newValue = ""
+        case 17 :   // /
+            saveValue = String(showResult.text!)
+            calculator = "/"
+            showResult.text = ""
+            newValue = ""
+        default:    // =
+            let tmp1 : Double = Double(saveValue)!
+            let tmp2 : Double = Double(newValue)!
+            if calculator == "+" {
+                result = plus(save : tmp1, new : tmp2)
+            } else if calculator == "-" {
+                result = minus(save : tmp1, new : tmp2)
+            } else if calculator == "*" {
+                result = multiply(save : tmp1, new : tmp2)
+            } else if calculator == "/" {
+                result = divide(save : tmp1, new : tmp2)
+            } else {
+            }
+            
+            showResult.text = String(result)
+            formula.append("= \(result)")
+            showFormula.text = formula
+            formula = ""
+        }
+        
+    }
+    
+    func plus(save : Double, new : Double) -> Double {
+        calculator = ""
+        return save + new
+    }
+    
+    func minus(save : Double, new : Double) -> Double {
+        calculator = ""
+        return save - new
+    }
+    
+    func multiply(save : Double, new : Double) -> Double {
+        calculator = ""
+        return save * new
+    }
+    
+    func divide(save : Double, new : Double) -> Double {
+        calculator = ""
+        return save / new
     }
 }
